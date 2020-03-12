483685c602ab4ab54cb34a74129bf9096053c38f
festplatte <mich.gabler@gmx.de>
2020-03-12 10:11:09 +0100

    Add 3d exercise

---
+++ 3d-images/composite.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+# config
+IMG1_PATH = 'img/left.jpg'
+IMG2_PATH = 'img/right.jpg'
+DEST_IMG_PATH = 'img/composite.jpg'
+
+# load images
+img1 = cv.imread(IMG1_PATH,1)
+img2 = cv.imread(IMG2_PATH,1)
+
+# copy images
+dest_img = np.zeros(img1.shape)
+dest_img[:,:,2] = img1[:,:,2]
+dest_img[:,:,1] = img2[:,:,1]
+dest_img[:,:,0] = img2[:,:,0]
+
+# save dest image
+cv.imwrite(DEST_IMG_PATH, dest_img)
+
+# show dest image
+plt.imshow(dest_img[...,::-1] / 255)
+plt.show()
---
+++ 3d-images/side-by-side.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+# config
+IMG1_PATH = 'img/left.jpg'
+IMG2_PATH = 'img/right.jpg'
+DEST_IMG_PATH = 'img/side-by-side.jpg'
+
+# load images
+img1 = cv.imread(IMG1_PATH,1)
+img2 = cv.imread(IMG2_PATH,1)
+
+# copy images
+dest_img = cv.hconcat([img1, img2])
+
+# save dest image
+cv.imwrite(DEST_IMG_PATH, dest_img)
+
+# show dest image
+plt.imshow(dest_img[...,::-1])
+plt.show()
